47d6bbf35f2f764e418a1a54d3639945c5e0bb03
pinasd <pinasd.99@gmail.com>
2017-05-17 16:47:42 +0800

    oh~Ya.V2

---
+++ php01.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/17
+ * Time: 下午 03:58
+ */
+/*
+ *
+ */
+
+echo 'Hi pinasd yo~ha<br>';
+echo 'Hi pinasd yo~ha';
+?>
+
+
---
+++ php2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/5/17
+ * Time: 下午 04:25
+ */
+$var1=123;
+$var2=12.3;
+$____=true;
+$ua=null;
+
+echo gettype($var1);
+echo("<br>");
+var_dump($var1);
+$var1=10;
+$var2=3;
+$var3=$var1/$var2;
+echo('<br>');
+var_dump($var3);
+
+
+?>
