4c10ab508a065631bb9b8b39d83718f18685561e
pinasd <pinasd.99@gmail.com>
2017-06-01 16:44:39 +0800

    12345678987654321

--- movediv.php
+++ movediv.php
@@ -1,11 +1,201 @@
 <html>
 
 <head>
+    <style>
 
+    #leftmenuinner {
+        position:fixed;
+        top:0;
+        padding-bottom:0;
+        height:100%;
+        width:20%;
+        background-color:transparent;
+        float:left;
+    }
+    #leftmenuinnerinner {
+        height:100%;
+        width:100%;
+        overflow-y:scroll;
+        overflow-x:hidden;
+        padding-top:60px;
+    }
+    </style>
+</head>
+<body>
+
+<nav >
+    <ul>
+        <li>nav 1</li>
+        <li>nav 2</li>
+    </ul>
+    <div id="nav1" >
+        <div id='leftmenuinner'>
+            <div  id='leftmenuinnerinner'>
+                <a href='javascript:void(0)' onclick='close_menu()'  style='right:16px;padding:3px 12px;font-weight:bold;'>&times;</a>
+                <h2 class="left"><span class="left_h2">Bootstrap Tutorial</span></h2>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>            <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>            <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>  <a target="_top" href="default.asp">BS HOME</a>
+                <a target="_top" href="bootstrap_get_started.asp">BS Get Started</a>
+                <br><br>
+            </div>
+        </div>
+    </div>
+</nav>
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+</button>
+<section>
+    <article  style="background-color:red ;position: relative;left:20%">
+        <div id="div">
+            article
+            <p>dsfafasdf</p>
+            <h1>dfdasfasdf</h1>
+
+        </div>
+
+    </article>
+
+</body>
+
+
+</html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Resizable - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <style>
+        #resizable { width: 150px; height: 100%; padding: 0.5em; }
+        #resizable h3 { text-align: center; margin: 0; }
+    </style>
+    <script src="./libs/js/jquery.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $( function() {
+            $( "#leftmenuinner" ).resizable();
+//            $( "#div" ).resizable();
+        } );
+    </script>
 </head>
 
 
 
+</body>
 </html>
-
 
